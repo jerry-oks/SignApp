39af9340dbfaeb91870a935b35e6020d4ea3a740
Vadim Vanatov <79990006611@yandex.ru>
2023-08-31 19:57:41 +0300

    Initial Commit

---
+++ SignApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignApp
+//
+//  Created by HOLY NADRUGANTIX on 31.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
